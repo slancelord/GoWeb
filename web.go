0b247485dfb8c67b89fc88d403dc86f08269f133
Zaritsky Sergey <slancelord@gmail.com>
2022-04-04 23:08:12 +0500

    creat

---
+++ web.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	file, err := os.Create(filename)
+
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(p.Body)
+	return err
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	file, err := os.Open(filename)
+	info, _ := os.Stat(filename)
+
+	body := make([]byte, info.Size())
+	_, err = file.Read(body)
+
+	return &Page{Title: title, Body: body}, err
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
